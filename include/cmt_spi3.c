3def3abe8ae7a7948ab4cbe783884cbe6d9c3967
SeByDocKy <sebydocky@hotmail.com>
2025-10-10 09:16:38 +0200

    Update cmt_spi3.c

--- include/cmt_spi3.c
+++ include/cmt_spi3.c
@@ -64,7 +64,17 @@ void cmt_spi3_init(const int8_t pin_sdio, const int8_t pin_clk, const int8_t pin
     };
     ESP_ERROR_CHECK(spi_bus_add_device(SPI_CMT, &devcfg2, &spi_fifo));
 
-    esp_rom_gpio_connect_out_signal(pin_sdio, spi_periph_signal[SPI_CMT].spid_out, true, false);
+    // esp_rom_gpio_connect_out_signal(pin_sdio, spi_periph_signal[SPI_CMT].spid_out, true, false);
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+    // Pour ESP-IDF 5.x : utiliser les constantes directes
+  #if SPI_CMT == 2
+    esp_rom_gpio_connect_out_signal(pin_sdio, SPI2_D_OUT_IDX, true, false);
+  #elif SPI_CMT == 3
+    esp_rom_gpio_connect_out_signal(pin_sdio, SPI3_D_OUT_IDX, true, false);
+  #endif
+#else
+    
     delay(100);
 }
 
