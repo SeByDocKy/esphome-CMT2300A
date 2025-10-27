eb10d2071c9fd059fc54a106824122a9348dffd1
SeByDocKy <sebydocky@hotmail.com>
2025-10-27 18:18:28 +0100

    Update cmt2300a.c

--- include/cmt2300a.c
+++ include/cmt2300a.c
@@ -741,7 +741,7 @@ bool CMT2300A_Init(void)
 {
     uint8_t tmp;
 
-    ESP_LOGI("CMT2300","enter here");
+    // ESP_LOGI("CMT2300","enter here");
 
     CMT2300A_SoftReset();
     CMT2300A_DelayMs(20);
